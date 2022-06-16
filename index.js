a757b33f8d60eeb0359d3ff426c8cdcba5a63627
Katsuro Alexandr <Katsuroay@mail.ru>
2022-06-16 12:46:38 +0300

    version 16.06.2022 12:47

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-let editButton = document.querySelector('.profile__rectangle-button');
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__info');
+let editButton = document.querySelector('.profile__rectangle-button')
+const popup = document.querySelector('.popup')
+const popupCloseButton = document.querySelector('.popup__close')
+let formElement = document.querySelector('.popup__container')
+let nameInput = formElement.querySelector('.popup__name')
+let jobInput = formElement.querySelector('.popup__info')
 let popupSubmitButton = document.querySelector('.popup__submit-button')
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+let profileTitle = document.querySelector('.profile__title')
+let profileSubtitle = document.querySelector('.profile__subtitle')
+let likeButton = document.querySelector('.elements__like')
+
 
 
 function openPopup(event) {
-    popup.classList.add('popup_opened');
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
+    popup.classList.add('popup_opened')
+    nameInput.value = profileTitle.textContent
+    jobInput.value = profileSubtitle.textContent
 }
-editButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', openPopup)
 
 
 function closePopup(event) {
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened')
 }
-popupCloseButton.addEventListener('click', closePopup);
+popupCloseButton.addEventListener('click', closePopup)
 
 
 function formSubmitHandler (evt) {
-    evt.preventDefault();
+    evt.preventDefault()
     profileTitle.textContent = nameInput.value
     profileSubtitle.textContent = jobInput.value
-    closePopup();
+    closePopup()
 }
-// console.log(formSubmitHandler())
+formElement.addEventListener('submit', formSubmitHandler)
 
 
-formElement.addEventListener('submit', formSubmitHandler);
+function likeActive(event) {
+    likeButton.classList.toggle('elements__like_active')
+}
 
 
-
-
+likeButton.addEventListener('click', likeActive)
 
 
